da18168e4adf4dbc37a1cebe8d788309e4975c72
nf6813144 <nf6813144@gmail.com>
2024-11-13 14:17:22 +0800

    Create fungsi_modularasi.py

---
+++ fungsi_modularasi.py
@@ -0,0 +1,9 @@
+#fungsi dan modularasi
+#f(x)=5x-10
+def fungsiku (x):
+    return (5*x)-10
+a=int(input("masukkan nilai a:"))
+
+#hasil fx jika nilai x = 10
+hasil_fx = fungsiku (a)
+print(hasil_fx)
